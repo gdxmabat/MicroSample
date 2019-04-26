2aa04236d6ef85de60794193b07c91d1a472dd31
Mario Medina Bartolome <mmedinab@everis.com>
2019-04-26 14:32:11 +0200

    clean house

--- MicroSample.Business/Repository/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Koa.Persistence.EntityRepository.EntityFrameworkCore.Mapper;
-using MicroSample.Domain.Entity;
-
-namespace MicroSample.Business.Repository
-{
-    public interface IUserRepository : IMapperRepository<User, int>
-    {
-    }
-}
